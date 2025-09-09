ca40dcee3c34ed998eed43a89333252cff9c835a
NDCongSP <ndcong08cddv02@gmail.com>
2025-09-09 18:04:38 +0700

    up

--- ATWebLoggerBVDHYD/ATWebLogger.Core/WebLogger.cs
+++ ATWebLoggerBVDHYD/ATWebLogger.Core/WebLogger.cs
@@ -68,8 +68,8 @@ namespace ATWebLogger.Core
         public Locations Locations { get; set; }
         public Alarms Alarms { get; set; }
 
-        //public string PathFile = @"C:\GatewayParametters\";
-        public string PathFile = $"/home/pi/";
+        public string PathFile = @"C:\GatewayParametters\";
+        //public string PathFile = $"/home/pi/";
 
         public double VotLo = 5;
         #endregion
